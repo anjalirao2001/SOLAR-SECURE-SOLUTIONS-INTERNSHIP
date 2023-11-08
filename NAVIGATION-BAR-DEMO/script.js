bf0600300501c0d4c6b37ed57c6468465af082d7
anjalirao2001 <anjali07rao@gmail.com>
2023-11-08 14:09:31 +0530

    Added navigation bar using html, css, js

---
+++ NAVIGATION-BAR-DEMO/script.js
@@ -0,0 +1,16 @@
+var navstatus = 0;
+function openNav()
+{
+    if(!navstatus)
+    {
+        $(`.maindiv`).css(`width` , `100%`)
+        $(`a`).css(`display` , `block`)
+        navstatus=1
+    }
+    else
+    {
+        $(`.maindiv`).css(`width` , `0`)
+        $(`a`).css(`display` , `none`)
+        navstatus=0
+    }
+}
