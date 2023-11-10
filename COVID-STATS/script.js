5cb827133cad626741bcd100400b6e887fca513d
anjalirao2001 <anjali07rao@gmail.com>
2023-11-10 14:42:57 +0530

    Added Code using html css javascript

--- COVID-STATS/script.js
+++ COVID-STATS/script.js
@@ -2,15 +2,15 @@ $.ajax({
 
     type : 'get',
     url : 'https://api.covid19api.com/summary',
-    success : function(responce)
+    success : function(response)
     {
-        console.log(responce.Countries)
+        console.log(response.Countries)
 
-        for(var i=0 ;i< responce.Countries.length ; i++)
+        for(var i=0 ;i< response.Countries.length ; i++)
         {
             // console.log(responce.Countries[i].Country)
-            var totalactive = responce.Countries[i].TotalConfirmed - responce.Countries[i].TotalRecovered
-            var tablerow = `<tr> <td> $(responce.Countries[i].Country)</td><td> $(responce.Countries[i].TotalConfirmed)</td><td> $(totalactive)</td><td> $(responce.Countries[i].TotalRecovered)</td><td> $(responce.Countries[i].TotalDeaths)</td> </tr>`
+            var totalactive = response.Countries[i].TotalConfirmed - response.Countries[i].TotalRecovered
+            var tablerow = `<tr> <td> $(response.Countries[i].Country)</td><td> $(response.Countries[i].TotalConfirmed)</td><td> $(totalactive)</td><td> $(response.Countries[i].TotalRecovered)</td><td> $(response.Countries[i].TotalDeaths)</td> </tr>`
             $('#tbody').append(tablerow)
         }
         $('#covidtalle').DataTable()
